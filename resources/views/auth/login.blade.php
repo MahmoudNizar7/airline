50c98a7587fda70fa9afe3b21191535556a5707b
MahmoudNizar7 <mabonima@hotmail.com>
2021-02-06 22:57:37 +0200

    add db

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,73 +1,231 @@
-@extends('layouts.app')
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href='https://fonts.googleapis.com/css?family=Work+Sans:400,300,700' rel='stylesheet' type='text/css'>
 
-@section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
+    <title>لوحة التحكم | شركة المصطفى للسياحة و السفر</title>
 
-                <div class="card-body">
-                    <form method="POST" action="{{ route('login') }}">
-                        @csrf
 
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
 
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <style>
+        @import url("https://fonts.googleapis.com/css?family=SourceSansPro");
+        * {
+            -webkit-box-sizing: border-box;
+            -moz-box-sizing: border-box;
+            box-sizing: border-box;
+        }
+        html,
+        body {
+            width: 100%;
+            height: 100%;
+            background: #f2f1ef;
+        }
+        h1,
+        h2,
+        h3,
+        h4,
+        h5,
+        h6,
+        p,
+        input,
+        button {
+            font-family: 'Source Sans Pro', sans-serif;
+        }
 
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
+        input {
+            width: 100%;
+            border: none;
+            margin: 0;
+            padding: 20px 30px;
+            font-size: 18px;
+            font-weight: 200;
+            outline: none;
+            background: none;
+        }
+        .login-form {
+            width: 360px;
+            position: absolute;
+            left: 50%;
+            top: 50%;
+            margin: -260px 0 0 -180px;
+        }
+        .login-form header {
+            background: #22313f;
+            color: #fff;
+            padding: 30px;
+            height: 300px;
+            overflow: hidden;
+            text-align: center;
+        }
+        .login-form header .photo,
+        .login-form header .user-info {
+            width: 100%;
+        }
+        .login-form header .photo {
+            margin-top: 30px;
+        }
+        .login-form header img {
+            border-radius: 50%;
+        }
+        .login-form header .user-info {
+            padding-top: 20px;
+        }
+        .login-form header .user-info h3,
+        .login-form header .user-info h5 {
+            font-weight: 200;
+        }
+        .login-form header .user-info h3 {
+            font-size: 36px;
+        }
+        .login-form header .user-info h5 {
+            font-size: 12px;
+            letter-spacing: 0.05em;
+        }
+        .login-form section .input,
+        .login-form section .password {
+            border-bottom: 1px solid #d2d7d3;
+            background: #fff;
+        }
+        .login-form section .password {
+            position: relative;
+        }
+        .login-form section .password .toggle-password {
+            font-size: 12px;
+            color: #d2d7d3;
+            border: 1px solid #d2d7d3;
+            border-radius: 4px;
+            padding: 4px 8px;
+            position: absolute;
+            right: 30px;
+            top: 20px;
+        }
+        .login-form section .password .toggle-password:hover {
+            background: #d2d7d3;
+            color: #fff;
+        }
+        .login-form section .confirm-password,
+        .login-form section .inner {
+            -webkit-transition: all 500ms cubic-bezier(0.645, 0.045, 0.355, 1);
+            -moz-transition: all 500ms cubic-bezier(0.645, 0.045, 0.355, 1);
+            -o-transition: all 500ms cubic-bezier(0.645, 0.045, 0.355, 1);
+            -ms-transition: all 500ms cubic-bezier(0.645, 0.045, 0.355, 1);
+            transition: all 500ms cubic-bezier(0.645, 0.045, 0.355, 1);
+        }
+        .login-form section .confirm-password {
+            position: relative;
+            -webkit-perspective: 1000px;
+            -moz-perspective: 1000px;
+            -ms-perspective: 1000px;
+            perspective: 1000px;
+            height: 64px;
+        }
+        .login-form section .inner {
+            height: 0px;
+            -webkit-transform-origin: top;
+            -moz-transform-origin: top;
+            -o-transform-origin: top;
+            -ms-transform-origin: top;
+            transform-origin: top;
+            -webkit-transform: rotateX(-90deg);
+            -moz-transform: rotateX(-90deg);
+            -o-transform: rotateX(-90deg);
+            -ms-transform: rotateX(-90deg);
+            transform: rotateX(-90deg);
+            background-color: #d2d7d3;
+        }
+        .login-form footer {
+            background: #fff;
+            overflow: hidden;
+            -webkit-transition: all 500ms cubic-bezier(0.645, 0.045, 0.355, 1);
+            -moz-transition: all 500ms cubic-bezier(0.645, 0.045, 0.355, 1);
+            -o-transition: all 500ms cubic-bezier(0.645, 0.045, 0.355, 1);
+            -ms-transition: all 500ms cubic-bezier(0.645, 0.045, 0.355, 1);
+            transition: all 500ms cubic-bezier(0.645, 0.045, 0.355, 1);
+            -webkit-transform: translate3d(0px, -64px, 0px);
+            -moz-transform: translate3d(0px, -64px, 0px);
+            -o-transform: translate3d(0px, -64px, 0px);
+            -ms-transform: translate3d(0px, -64px, 0px);
+            transform: translate3d(0px, -64px, 0px);
+        }
+        .login-form footer a {
+            color: #d2d7d3;
+            float: left;
+            height: 40px;
+            line-height: 0px;
+            padding: 30px;
+        }
+        .login-form footer .action a {
+            background: #22a7f0;
+            color: #fff;
+            float: right;
+            padding: 30px 60px;
+        }
+        .login-form footer .action a:hover {
+            background: #38b0f2;
+        }
+        .login-form.confirming .inner {
+            height: 64px;
+            border-bottom: 1px solid #f2f1ef;
+            -webkit-transform: rotateX(0deg);
+            -moz-transform: rotateX(0deg);
+            -o-transform: rotateX(0deg);
+            -ms-transform: rotateX(0deg);
+            transform: rotateX(0deg);
+            background-color: #fff;
+        }
+        .login-form.confirming footer {
+            border-top: 1px solid #d2d7d3;
+            -webkit-transform: translate3d(0px, -1px, 0px);
+            -moz-transform: translate3d(0px, -1px, 0px);
+            -o-transform: translate3d(0px, -1px, 0px);
+            -ms-transform: translate3d(0px, -1px, 0px);
+            transform: translate3d(0px, -1px, 0px);
+        }
 
-                        <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
+    </style>
+</head>
+<body>
 
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
+<div class="login-form">
+    <form action="{{ route('login') }}" method="post">
+        @csrf
+        @php
+            $image = \App\Models\Control\Setting::where('key', 'image')->first();
+        @endphp
+        <header>
+            <div class="photo"><img style="margin-top:-60px " src="{{ asset('assets/images/'.$image->value) }}" alt=""/></div>
+        </header>
+        <section>
+            <div class="input">
+                <input name="email" class="@error('email') is-invalid @enderror" type="email" placeholder="البريد الإلكتروني"/>
+                @error('email')<span
+                    class="invalid-feedback" style="color: red;"><strong>{{ __($message) }}</strong></span>@enderror
+            </div>
+            <div class="password">
+                <input name="password" class="@error('password') is-invalid @enderror" type="password" placeholder="كلمة المرور" style="">
+                @error('password')<span
+                    class="invalid-feedback" style="color: red"><strong>{{ __($message) }}</strong></span>@enderror
 
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
+            </div>
+            <div class="confirm-password">
+                <div class="inner">
+                    <input placeholder=""/>
 
-                        <div class="form-group row">
-                            <div class="col-md-6 offset-md-4">
-                                <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
-
-                                    <label class="form-check-label" for="remember">
-                                        {{ __('Remember Me') }}
-                                    </label>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="form-group row mb-0">
-                            <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Login') }}
-                                </button>
-
-                                @if (Route::has('password.request'))
-                                    <a class="btn btn-link" href="{{ route('password.request') }}">
-                                        {{ __('Forgot Your Password?') }}
-                                    </a>
-                                @endif
-                            </div>
-                        </div>
-                    </form>
                 </div>
             </div>
-        </div>
-    </div>
+        </section>
+        <footer>
+            <div class="action"><input type="submit" class="btn btn-info save form-group form-check-input" style="font-size: 20px;font-family: 'Cairo'" value="دخول"></div>
+        </footer>
+    </form>
 </div>
-@endsection
+
+</body>
+</html>
